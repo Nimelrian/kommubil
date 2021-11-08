76012df3ce52b88c5b4f0e2c68745fb3f7314bb9
Sebastian Kappen <sebastian@kappen.dev>
2021-11-08 20:27:40 +0100

    Add embedded Kafka to source/logger services

--- bus-location-logger/src/test/kotlin/de/nimelrian/kommubil/buslocationlogger/BusLocationLoggerApplicationTests.kt
+++ bus-location-logger/src/test/kotlin/de/nimelrian/kommubil/buslocationlogger/BusLocationLoggerApplicationTests.kt
@@ -2,8 +2,15 @@ package de.nimelrian.kommubil.buslocationlogger
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(
+    partitions = 1,
+    brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"]
+)
 class BusLocationLoggerApplicationTests {
 
     @Test
