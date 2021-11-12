e420c75593e0a3e36f046335f792439668925512
Sebastian Kappen <sebastian@kappen.dev>
2021-11-12 16:50:32 +0100

    Use JettyWebSocket for bus location source

--- bus-location-source/src/main/kotlin/de/nimelrian/kommubil/buslocationsource/BusLocationSourceApplication.kt
+++ bus-location-source/src/main/kotlin/de/nimelrian/kommubil/buslocationsource/BusLocationSourceApplication.kt
@@ -1,53 +1,40 @@
 package de.nimelrian.kommubil.buslocationsource
 
 import mu.KotlinLogging
-import org.springframework.beans.factory.DisposableBean
+import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.function.context.PollableBean
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Component
-import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient
-import reactor.core.Disposable
+import org.springframework.integration.dsl.IntegrationFlows
+import org.springframework.integration.websocket.ClientWebSocketContainer
+import org.springframework.integration.websocket.IntegrationWebSocketContainer
+import org.springframework.integration.websocket.inbound.WebSocketInboundChannelAdapter
+import org.springframework.messaging.Message
+import org.springframework.web.socket.client.jetty.JettyWebSocketClient
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Sinks
-import java.net.URI
 import java.util.function.Supplier
 
 
 private val log = KotlinLogging.logger {}
 
-@Component
-class WebSocketSession(val websocketPayloads: Sinks.Many<String>) : DisposableBean {
-    private val socketSubscription: Disposable
-
-    init {
-        val client = ReactorNettyWebSocketClient()
-        val webSocketUri = URI.create("wss://websocket.busradar.conterra.de")
-
-        socketSubscription = client.execute(webSocketUri) { session ->
-            session.receive()
-                .doOnEach { message ->
-                    message.get()?.let { websocketPayloads.tryEmitNext(it.payloadAsText) }
-                }
-                .then()
-        }.subscribe()
-    }
-
-    override fun destroy() {
-        log.info { "Closing websocket" }
-        socketSubscription.dispose()
-    }
-}
-
 @SpringBootApplication
 class BusLocationSourceApplication {
     @Bean
-    fun websocketPayloads(): Sinks.Many<String> = Sinks.many().unicast().onBackpressureBuffer()
+    fun webSocketClient() = JettyWebSocketClient()
+
+    @Bean
+    fun clientWebSocketContainer(): IntegrationWebSocketContainer {
+        return ClientWebSocketContainer(webSocketClient(), "wss://websocket.busradar.conterra.de")
+    }
+
+    @Bean
+    fun payloadSupplierFlow(webSocketContainer: IntegrationWebSocketContainer): Publisher<Message<ByteArray>> =
+        IntegrationFlows.from(WebSocketInboundChannelAdapter(webSocketContainer)).log().toReactivePublisher()
 
     @PollableBean
-    fun locationSupplier(websocketPayloads: Sinks.Many<String>) = Supplier<Flux<String>> {
-        websocketPayloads.asFlux()
+    fun locationSupplier(payloadSupplier: Publisher<Message<ByteArray>>) = Supplier {
+        Flux.from(payloadSupplier)
     }
 }
 
